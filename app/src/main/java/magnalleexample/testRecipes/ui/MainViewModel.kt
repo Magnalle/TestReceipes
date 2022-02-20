cf9aaa18bf333d1abdd1ef87ee2f88d7a80d2cb9
Valentina <yasu@list.ru>
2022-02-21 06:30:25 +0700

    Scope fix

--- app/src/main/java/magnalleexample/testRecipes/ui/MainViewModel.kt
+++ app/src/main/java/magnalleexample/testRecipes/ui/MainViewModel.kt
@@ -2,31 +2,30 @@ package magnalleexample.testRecipes.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import magnalleexample.testRecipes.Repo.Repo
 import magnalleexample.testRecipes.domain.Recipe
+
 
 class MainViewModel : ViewModel() {
     lateinit var repo: Repo
     val errorText : MutableLiveData<String> = MutableLiveData("")
     val recipesList : MutableLiveData<List<Recipe>> = MutableLiveData(listOf())
     val isLoading : MutableLiveData<Boolean> = MutableLiveData(false)
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
 
     //функция получения рецептов из репозитория
     //выполняется в потоке IO
-    fun getRecipes() {
+    fun getRecipes(){
         isLoading.postValue(true)
-        GlobalScope.launch(
-            Dispatchers.IO){
+        scope.launch(Dispatchers.IO) {
             repo.getRecipesAsync({ list ->
                 isLoading.postValue(false)
                 recipesList.postValue(list)
-                                 }, { str ->
+            }, { str ->
                 isLoading.postValue(false)
                 errorText.postValue(str)
-                                 })
+            })
         }
     }
 }
